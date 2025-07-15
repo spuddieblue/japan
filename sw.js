a8f702610a6eb9710d6a61384a11689707905a96
spuddieblue <mike.duignan@gmail.com>
2025-07-15 11:46:30 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'japan-plan-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './itinerary.html',
+  './calculator.html',
+  './gifts.html',
+  './list.html',
+  './images/japan_plan.png',
+  './images/japanIcon.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.log('Cache addAll failed:', error);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
