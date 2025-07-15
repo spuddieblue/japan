6b18cda76d12a26b2372b0ae2a0ed6c0ef769a09
spuddieblue <mike.duignan@gmail.com>
2025-07-15 11:53:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,55 @@
 const CACHE_NAME = 'japan-plan-v1';
-const urlsToCache = [
+// Add all the files you want to cache.
+const URLS_TO_CACHE = [
   './',
   './index.html',
   './itinerary.html',
   './calculator.html',
-  './gifts.html',
   './list.html',
+  './gifts.html',
+  './manifest.json',
   './images/japan_plan.png',
   './images/japanIcon.png'
 ];
 
-// Install event
+// Install the service worker and cache the app shell.
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch((error) => {
-        console.log('Cache addAll failed:', error);
+        return cache.addAll(URLS_TO_CACHE);
       })
   );
 });
 
-// Fetch event
+// Fetch event: serve cached content when offline.
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
+        // Cache hit - return response
         if (response) {
           return response;
         }
+        // Not in cache - fetch from network
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
 
-// Activate event
+// Activate event: clean up old caches.
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating...');
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
